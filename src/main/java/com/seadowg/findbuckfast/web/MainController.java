913ab835df58dab118a90644259ccd3e5b5de3aa
Callum Stott <callum@seadowg.com>
2015-01-16 14:38:03 +0000

    Include handlebars

--- src/main/java/com/seadowg/findbuckfast/web/MainController.java
+++ src/main/java/com/seadowg/findbuckfast/web/MainController.java
@@ -1,16 +1,23 @@
 package com.seadowg.findbuckfast.web;
 
+import com.github.jknack.handlebars.Handlebars;
+import com.github.jknack.handlebars.Template;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
 
 @Controller
 public class MainController {
 
     @RequestMapping("/")
     @ResponseBody
-    public String home() {
-        return "Hello, Buckfast!";
+    public String home() throws IOException {
+        Handlebars handlebars = new Handlebars();
+
+        Template template = handlebars.compileInline("Hello, {{this}}!");
+        return template.apply("Buckfast");
     }
 
 }
