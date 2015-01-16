ab62995eb8fbec9a02ae825666bc98d079e431a6
Callum Stott <callum@seadowg.com>
2015-01-16 15:31:21 +0000

    Add redis lawl

--- src/main/java/com/seadowg/findbuckfast/web/MainController.java
+++ src/main/java/com/seadowg/findbuckfast/web/MainController.java
@@ -5,6 +5,7 @@ import com.github.jknack.handlebars.Template;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import redis.clients.jedis.Jedis;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -16,11 +17,15 @@ public class MainController {
     @RequestMapping("/")
     @ResponseBody
     public String home() throws IOException {
-        Handlebars handlebars = new Handlebars();
-        Template homeTemplate = handlebars.compile("home");
+        try(Jedis jedis = new Jedis("localhost")) {
+            Long visitorsCount = jedis.incr("visitors_count");
 
-        Map map = new HashMap();
-        map.put("count", 0);
-        return homeTemplate.apply(map);
+            Handlebars handlebars = new Handlebars();
+            Template homeTemplate = handlebars.compile("home");
+
+            Map map = new HashMap();
+            map.put("count", visitorsCount);
+            return homeTemplate.apply(map);
+        }
     }
 }
